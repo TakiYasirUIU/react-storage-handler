d180583f5d5dcf0c803484e5ce0ff8908d866245
TakiYasirUIU <yasirtaki1995@gmail.com>
2022-06-23 01:23:03 +0600

    store multiple data in an object with local storage

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import './Cosmetic.css'
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
     const addToCart = (id) =>{
-        console.log('item added', id)
+        addToDb(id);
     }
-    const addToCartWtihParam = () => addToCart(id)
+    // const addToCartWtihParam = () => addToCart(id)
     return (
         <div className='product'>
             <h2>Buy this: {name}</h2>
             <p>Only for: ${price}</p>
             <p><small>it has id: {id}</small></p>
-            <button onClick={addToCartWtihParam}>Add to Cart</button>
+            {/* <button onClick={addToCartWtihParam}>Add to Cart</button> */}
             {/* Short cut */}
-            <button onClick={() => addToCart(id)}>Purchase</button>
+            <button onClick={() => addToCart(id)}>Add to Cart</button>
         </div>
     );
 };
